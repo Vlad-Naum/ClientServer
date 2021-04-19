550ba2e1d9d9fc81e56f7fa509e94132d1e5832d
Vlad <naumkin.vlad2012@yandex.ru>
2021-04-19 20:22:57 +0300

    Update Server.1

--- src/Client.java
+++ src/Client.java
@@ -2,15 +2,19 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        Socket clientSocket = new Socket("127.0.0.1", 8000);
-        OutputStreamWriter osw = new OutputStreamWriter(clientSocket.getOutputStream());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    public static void main(String[] args) {
+        try (Socket clientSocket = new Socket("127.0.0.1", 8000);
+             OutputStreamWriter osw = new OutputStreamWriter(clientSocket.getOutputStream());
+             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
 
-        osw.write("Hello Server.");
-
-
-        osw.close();
-        clientSocket.close();
+            osw.write("Hello Server." + "\n");
+            osw.flush();
+            String str;
+            while ((str = reader.readLine()) != null) {
+                System.out.println(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -5,24 +5,44 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8000);
+        System.out.println("Server started.");
+        int number = 0;
 
-        Socket client = null;
-        OutputStreamWriter osw = null;
-        BufferedReader reader = null;
-        int t = 0;
         while (true) {
-            client = serverSocket.accept();
-            osw = new OutputStreamWriter(client.getOutputStream());
-            reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            Socket client = serverSocket.accept();
+            Thr thr = new Thr(client, ++number);
+            thr.start();
+        }
+    }
+}
+
+class Thr extends Thread{
+    private Socket client;
+    private int number;
+
+    public Thr(Socket client, int number) {
+        this.client = client;
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        try (OutputStreamWriter osw = new OutputStreamWriter(client.getOutputStream());
+             BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()))){
+
+            String message = reader.readLine();
+            System.out.println("Client #" + number + ". Your message: " + message);
 
             osw.write("HTTP/1.0 200 OK\n" +
                     "Content-type: text/html\n" +
                     "\n" +
-                    "Hello! You are #" + t);
-            String message = reader.readLine();
-            System.out.println("Client #" + ++t + ". Your message: " + message);
+                    "Hello! You are #" + number + "\n");
             osw.flush();
-            osw.close();
+            number++;
+
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
